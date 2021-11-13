71da5d057b21dbbfbd32a4949c4b1bea4825ca7b
Chams AGOUNI <agounichams1@gmail.com>
2021-11-13 10:10:43 -0800

    Fixed #2 - Unsupported lookup 'ilike' for Field on filterSearch

--- athena/core/apps.py
+++ athena/core/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.db.models import Field
+
+from athena.core.db.filters import PostgresILike
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'athena.core'
+
+    def ready(self):
+        Field.register_lookup(PostgresILike)
